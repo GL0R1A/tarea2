54f06293866eac1724d05b0a05c9cba98ea41493
GL0R1A <79273578+GL0R1A@users.noreply.github.com>
2021-02-19 16:07:33 -0600

    Tarea
    Ejercicios

---
+++ mayu/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Mayus
+{
+    class Mayus
+    {
+        static void Main(string[] args)
+        {
+            string let;
+            Console.WriteLine("Escriba cualquier letra: \n");
+            let = Console.ReadLine();
+
+
+            if (let == "A" || let == "B" || let == "C" || let == "D" || let == "E" ||
+                let == "F" || let == "G" || let == "H" || let == "I" || let == "J" ||
+                let == "K" || let == "L" || let == "M" || let == "N" || let == "Ñ" ||
+                let == "O" || let == "P" || let == "Q" || let == "R" || let == "S" ||
+                let == "T" || let == "U" || let == "V" || let == "W" || let == "X" ||
+                let == "Y" || let == "Z") { Console.WriteLine(" SI ES MAYUSCULA "); }
+            else
+            {
+                Console.WriteLine("NO ES MAYUSCULA :");
+                Console.WriteLine();
+                Console.Write("Presione una tecla para terminar . . . ");
+                Console.ReadKey();
+            }
+        }
+    }
+
